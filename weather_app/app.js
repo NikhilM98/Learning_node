3852da9ff7657311d23dd01ee152ca523268ecda
NikhilM98 <nikhilmehra998@gmail.com>
2018-01-10 14:49:17 +0530

    Chapter3 Lesson8

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,9 +1,43 @@
 const request = require('request');
+const yargs = require('yargs');
 
-request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=IIT%20Roorkee',
-    json: true,
-},
-(error, response, body) => {
-    console.log(body);
-});
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true,
+        },
+        
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+// encodeURIComponent decodeURIComponent
+
+// console.log(argv);
+getAddress = (argv) => {
+    request({
+        url: 'https://maps.googleapis.com/maps/api/geocode/json?address='+encodeURIComponent(argv.a),
+        json: true,
+    },
+    (error, response, body) => {
+        if (error) {
+            console.log('Unable to connect with Google Servers');
+        } else if (body.status === 'ZERO_RESULTS') {
+            console.log ('Invalid Address');
+        } else if (body.status === 'OVER_QUERY_LIMIT') {
+            console.log('Failed to get address (OVER_QUERY_LIMIT). Retrying...');
+            setTimeout(() => getAddress(argv), 1000);
+        } else if (body.status === 'OK') {
+            // console.log(JSON.stringify(body, undefined, 2));
+            console.log(`The Address is ${body.results[0].formatted_address}`);
+            console.log(`The Lat is ${body.results[0].geometry.location.lat}`);
+            console.log(`The Lng is ${body.results[0].geometry.location.lng}`);
+        }
+    });
+}
+
+getAddress(argv);
