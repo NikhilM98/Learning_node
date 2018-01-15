801ae9ad033069f01c06f7d1e411715664bf048c
NikhilM98 <nikhilmehra998@gmail.com>
2018-01-15 19:38:52 +0530

    hbs partials, hepler and express middlewares

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+ var now = new Date().toString();
+ var log = `${now}: ${req.method} ${req.url}`
+ console.log(log);
+ fs.appendFile('server.log', log + '\n', err => {if (err) console.log(err)});
+ next();
+});
+
+// Maintainance Page, remove next() to apply
+app.use((req, res, next) => {
+    res.render('maintainance.hbs', {
+        pageTitle: 'Maintainance Page',
+        welcomeMessage: 'The site is currently under Maintainance'
+    })
+})
+
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', text => {
+    return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
     // res.send('<h1>Hello Express!</h1>');
@@ -19,7 +46,6 @@ app.get('/', (req, res) => {
     // });
     res.render('home.hbs', {
         pageTitle: 'About Page',
-        currentYear: new Date().getFullYear(),
         welcomeMessage: 'Welcome to my learning node homepage'
     })
 });
@@ -27,8 +53,7 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     // res.send('About Page');
     res.render('about.hbs', {
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'About Page'
     });
 });
 
